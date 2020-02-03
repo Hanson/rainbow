d08e68acb4bc6afe51a148ea781486cebe1ad1ae
Hanson <h@hanc.cc>
2020-02-03 14:06:38 +0800

    first commit

---
+++ src/Rainbow.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Hanson;
+
+use Encore\Admin\Extension;
+
+class Rainbow extends Extension
+{
+    public $name = 'rainbow';
+
+    public $assets = __DIR__.'/../resources/assets';
+}
---
+++ src/RainbowServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Hanson;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class RainbowServiceProvider extends ServiceProvider
+{
+    public function boot(Rainbow $extension)
+    {
+        if (!Rainbow::boot()) {
+            return;
+        }
+
+        $cssPath = 'vendor/laravel-admin/AdminLTE/dist/css/';
+
+        if ($this->app->runningInConsole() && $assets = $extension->assets()) {
+            $this->publishes(
+                [$assets => public_path($cssPath)],
+                'rainbow'
+            );
+        }
+    }
+}
